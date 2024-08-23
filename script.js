22c494d3f6e9d4c7065a5011cea5f7c13476406d
ingridcriistine <ingridrochacristine@gmail.com>
2024-08-22 22:28:10 -0300

    updates

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
     // inicializa os elementos
     function init() {
         for(var i = 1; i < 9; i++) {
-            var peca = document.getElementById("#peca" + i);
+            var peca = document.querySelector("#peca" + i);
             peca.addEventListener("click", movePeca);
             pecas.push(peca);
         }
 
-        pecas.push(null);
+        pecas.push(null); //peça em branco para o movimento
         answer = pecas;
 
         render();
@@ -24,14 +24,63 @@
 
             var peca = pecas[i];
 
+            // definição da posição de cada peça
             if(peca) {
 
-            }
+				peca.style.left = (i%3) * 225 + 10 + "px"; 
+
+				if(i < 3) {
+					peca.style.top = "15px";
+				} else if(i < 6) {
+					peca.style.top = "245px";
+				} else {
+					peca.style.top = "475px"
+				}
+			}
         }
     }
 
     function movePeca() {
 
+    }
+
+    function randomGame(model) {
+
+        var newModel;
+        var count = 0;
+
+        do {
+
+            newModel = [];
+
+            while(newModel.length < model.length) {
+                var i = Math.floor(Math.random() * model.length) //número aleatório entre 0 e 8
+            
+                // verifica se a posição está livre
+                if(newModel.indexOf(model[i] < 0)) {
+                    newModel.push(model[i]);
+                }
+            }
+
+            count++;
+            console.log(count); //verifica quantos jogos foram criados até ter uma ordem válida
+        
+        } while(!verify(newModel));
+
+        return newModel;
+    }
+
+    function verify() {
+        
+        for(var i in pecas) {
+            
+            var a = pecas[i];
+            var b = answer[i];
+
+            if(a != b) {return false;}
+        }
+
+        return true;
     }
 
     function start () {
@@ -42,6 +91,7 @@
 
     }
 
-}
 
-)
+    init();
+
+} ());
